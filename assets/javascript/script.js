c4a9ad87193ad712ede2d7d4f926072b06f9d674
Schris <tovechris48@gmail.com>
2022-04-11 22:13:53 -0400

    first-time-box

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,5 @@
 var events = {}
+var today = document.querySelector("#currentDay");
 
 let date = new Date()
 let day = date.getDate();
@@ -7,3 +8,19 @@ let year = date.getFullYear();
 
 let fullDate = `${day}-${month}-${year}`;
 console.log(fullDate);
+
+$(".row").on("click", "div", function() {
+  var text = $(this)
+  .text()
+  .trim();
+
+  var textInput = $("<textarea>")
+  .addClass("formal-control")
+  .val(text);
+  $(this).replaceWith(textInput);
+  console.log(textInput);
+});  
+
+// var createCurrentDayEl = function() {
+//   var 
+// }
